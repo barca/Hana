db5980e29366c7bedfacb086bf0ec346cac20214
Joshua <jlee14@wesleyan.edu>
2014-10-26 18:03:48 -0400

    populated db with lessons

--- features/step_definitions/lessons_steps.rb
+++ features/step_definitions/lessons_steps.rb
@@ -1,33 +1,9 @@
-require 'uri'
-require 'cgi'
-require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "paths"))
-require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "selectors"))
+# Add a declarative step here for populating the DB with lessons.
 
-module WithinHelpers
-  def with_scope(locator)
-    locator ? within(*selector_for(locator)) { yield } : yield
+Given /the following lessons exist/ do |lessons_table|
+  lessons_table.hashes.each do |lesson|
+    # each returned element will be a hash whose key is the table header.
+    # you should arrange to add that movie to the database here.
+  Lesson.create!(lesson)
   end
-end
-World(WithinHelpers)
-
-When /^(?:|I )go to (.+)$/ do |page_name|
-  visit path_to(page_name)
-end
-
-When /^(?:|I )fill in "([^"]*)" with "([^"]*)"$/ do |field, value|
-  fill_in(field, :with => value)
-end
-
-Then /^(?:|I )should see \/([^\/]*)\/$/ do |regexp|
-  regexp = Regexp.new(regexp)
-
-  if page.respond_to? :should
-    page.should have_xpath('//*', :text => regexp)
-  else
-    assert page.has_xpath?('//*', :text => regexp)
-  end
-end
-
-When /^(?:|I )press "([^"]*)"$/ do |button|
-  click_button(button)
 end
