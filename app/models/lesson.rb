a9eb65e9de71b8b4032ae8897782cfe991a98133
Joshua Lee <jlee14@wesleyan.edu>
2014-11-09 16:20:13 -0500

    Fix bugs for iteration 2-1

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -6,10 +6,17 @@ class Lesson < ActiveRecord::Base
 	validates :title, :instructor, :grade_level, :location, presence: true
 	validates :max_enrollment, numericality: { only_integer: true }
 	validate :ends_at_after_starts_at
+	validate :enrollment_not_negative
 
 	def ends_at_after_starts_at
 		if ends_at < starts_at
 			errors.add(:ends_at, "can't be before Starts at")
+		end
+	end
+
+	def enrollment_not_negative
+		if max_enrollment < 1
+			errors.add(:max_enrollment, "must be a positive value")
 		end
 	end
 end
