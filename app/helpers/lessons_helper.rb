b5b791b1f3b087b7533556e0ec6b8c9d4e483729
Alex <adrexler@wesleyan.edu>
2014-11-16 22:51:14 -0500

    fixed error for repetitions in lessonsOn helper

--- app/helpers/lessons_helper.rb
+++ app/helpers/lessons_helper.rb
@@ -1,26 +1,20 @@
 module LessonsHelper
-	def lessonsOn(date, lessons)
-    lessons = lessons.where("\"lessons\".\"starts_at\" < ?", date + 1)
-    lessons = lessons.where("\"lessons\".\"end_date\" > ?" , date - 1)
+	def lessonsOn(date, classes, grade_level)
+		dayTomorrow = (date + 1)
+		dayYesterday = (date - 1)
+    	classes = classes.where("\"lessons\".\"starts_at\" < ?", date + 1)
+    	classes = classes.where("\"lessons\".\"end_date\" > ?" , date - 1)
+
 		days_of_week = ["sun","mon", "tue", "wed", "thu", "fri","sat"]
 		dayOfWeek = days_of_week[(date.wday)]
 		if dayOfWeek == "sun" || dayOfWeek == "sat"
-			return lessons.where(starts_at: date.beginning_of_day..date.end_of_day)
+			return classes.where(starts_at: date.beginning_of_day..date.end_of_day)
 		else
 			dayStart = "#{date.strftime("%Y-%m-%d")} 05:00:00.000000"
 			dayTomorrow = date + 1
 			dayEnd = "#{dayTomorrow.strftime("%Y-%m-%d")} 04:59:59.999999"
 
-			return lessons.find_by_sql("SELECT \"lessons\".* FROM \"lessons\"  WHERE (\"lessons\".\"starts_at\" BETWEEN \'#{dayStart}\' AND \'#{dayEnd}\') OR \"lessons\".\"#{dayOfWeek}\" = \'t\'")
-
-			#Lesson.where(starts_at: Date.today.beginning_of_day..Date.today.end_of_day)
-			#SELECT "lessons".* FROM "lessons"  WHERE "lessons"."mon" = 't'
-			#SELECT "lessons".* FROM "lessons"  WHERE ("lessons"."starts_at" BETWEEN '' '2014-11-16 05:00:00.000000' AND '2014-11-17 04:59:59.999999') OR
-			#return lessons.find_by_sql("SELECT * FROM lessons WHERE #{dayOfWeek} = true OR starts_at BETWEEN #{date.beginning_of_day} AND #{date.end_of_day}")
-			#lessonsOnDate = lessons.where(starts_at: date.beginning_of_day..date.end_of_day)
-			#return lessons.where(dayOfWeek.to_sym => true)
-			#Lesson.where(mon => true)
-			#return lessonsOnDate.merge(lessonsOnDay)
+			return Lesson.find_by_sql("SELECT \"lessons\".* FROM \"lessons\"  WHERE (\"lessons\".\"#{grade_level}\" = \'t\' AND (\"lessons\".\"starts_at\" < \'#{dayTomorrow}\') AND (\"lessons\".\"end_date\" > \'#{dayYesterday}\')) AND ((\"lessons\".\"starts_at\" BETWEEN \'#{dayStart}\' AND \'#{dayEnd}\') OR \"lessons\".\"#{dayOfWeek}\" = \'t\')")
 		end
 	end
 
