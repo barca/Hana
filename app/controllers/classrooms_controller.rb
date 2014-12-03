dcfba55004745218dc8c9d771b426d18afe0f566
Joshua Lee <jlee14@wesleyan.edu>
2014-12-02 22:47:55 -0500

    FINAL VERSION. ALL TESTS UPDATED

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -73,76 +73,15 @@ class ClassroomsController < ApplicationController
     end
   end
 
-  def available
-    sort = params[:sort] || session[:sort]
-    case sort
-    when 'name'
-      ordering = :name
-    when 'occupancy'
-      ordering = 'max_occupancy DESC'
-    end
-    
-    if params[:sort] != session[:sort]
-      session[:sort] = sort
-    end
-
-    if params[:day] != nil
-      @date = Date.new(params[:day]['get(1i)'].to_i,params[:day]['get(2i)'].to_i,params[:day]['get(3i)'].to_i)
-    else
-      @date = Date.today
-    end
-
-    if params[:starts_at] != nil
-      @start_time = Time.parse "2000-01-01 #{params[:starts_at]['get(4i)']}:#{params[:starts_at]['get(5i)']}"
-    else
-      @start_time = Time.now
-    end
-
-    if params[:ends_at] != nil
-      @end_time = Time.parse "2000-01-01 #{params[:ends_at]['get(4i)']}:#{params[:ends_at]['get(5i)']}"
-    else
-      @end_time = Time.now
-    end
-
-    @classrooms = Classroom.all
-    @available_classrooms = dailyAvailableClassrooms(@date)
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_classroom
       @classroom = Classroom.find(params[:id])
     end
 
-    def dailyAvailableClassrooms(date)
-      availables = []
-      allRooms = Classroom.all
-      allRooms.each do |classroom|
-        if classNotOccupiedOnDay(classroom, date)
-          availables << classroom
-        end
-      end
-      return availables
-    end
-
 
     def classNotOccupiedAtTime(classroom, date, start, finish)
       #build of off classNotOccupiedOnDay
-    end
-
-    def classNotOccupiedOnDay(classroom, date)
-      days_of_week = ["sun","mon", "tue", "wed", "thu", "fri","sat"]
-      dayOfWeek = days_of_week[(date.wday)]
-      #currently only returns classes available on a specific day
-      @lessons=Lesson.find_by_sql("SELECT * FROM \"lessons\"  
-                        WHERE  (\"lessons\".\"start_date\" = \'#{date}\'
-                                OR \"lessons\".\"#{dayOfWeek}\" = \'t\')
-                            AND \"lessons\".\"location\" == \'#{classroom.name}\'")
-      if @lessons == []
-        return true
-      else
-        return false
-      end      
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
