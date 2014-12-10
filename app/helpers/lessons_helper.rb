6cd2bf65085cc89df46b399e8a22f91a97e20a3f
barca <tharden@wesleyan.edu>
2014-12-10 00:34:46 -0500

    some single quotes may have been the issue

--- app/helpers/lessons_helper.rb
+++ app/helpers/lessons_helper.rb
@@ -17,26 +17,27 @@ module LessonsHelper
 			dayTomorrow = date + 1
 			dayEnd = "#{dayTomorrow.strftime("%Y-%m-%d")} 04:59:59.999999"
 			#SQL to return lessons for the appropriate grade level that occur on a given day
-			return classes.find_by_sql("SELECT * FROM \"lessons\"  
+			return classes.find_by_sql("SELECT * FROM \"lessons\"
 												WHERE (
 													\"lessons\".\"#{grade_level}\" = \'t\'
-												) 
+												)
 												AND (
 														(
 															(
-																\"lessons\".\"start_date\" < \'#{dayTomorrow}\'
-															) 
+																\"lessons\".\"start_date\" < \"#{dayTomorrow}\"
+															)
 															AND (
-																\"lessons\".\"end_date\" > \'#{dayYesterday}\'
-															) 
+																\"lessons\".\"end_date\" > \"#{dayYesterday}\"
+															)
 															AND (
 																\"lessons\".\"#{dayOfWeek}\" = \'t\'
 															)
-														)		
+														)
 														OR (
-															\"lessons\".\"start_date\" = \'#{date}\'
+															\"lessons\".\"start_date\" = \"#{date}\"
 														)
 													)
+                          #ORDER BY TIME doesn't work on PostGres
 												ORDER BY TIME(\"lessons\".\"starts_at\")")
 		end
 	end
@@ -61,7 +62,7 @@ module LessonsHelper
       days_of_week = ["sun","mon", "tue", "wed", "thu", "fri","sat"]
       dayOfWeek = days_of_week[(date.wday)]
       #currently only returns classes available on a specific day
-      @lessons=Lesson.find_by_sql("SELECT * FROM \"lessons\"  
+      @lessons=Lesson.find_by_sql("SELECT * FROM \"lessons\"
                         WHERE  (\"lessons\".\"start_date\" = \'#{date}\'
                                 OR (\"lessons\".\"#{dayOfWeek}\" = \'t\' AND \"lessons\".\"start_date\" < \'#{date+1}\' AND \"lessons\".\"end_date\" > \'#{date-1}\'))
                             AND \"lessons\".\"location\" == \'#{classroom.name}\'")
@@ -69,7 +70,7 @@ module LessonsHelper
         return true
       else
         return false
-      end      
+      end
     end
 
 	def getLessonColor(lesson)
@@ -77,8 +78,8 @@ module LessonsHelper
 		if lessons_classroom.empty?
 			"black"
 		else
-			lessons_classroom[0].color 
+			lessons_classroom[0].color
 		end
 	end
 
-end 
+end
